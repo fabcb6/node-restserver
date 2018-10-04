a46e14db408374702048f1d09e0eaefd39db273a
Fabian Campos <fabcb6@gmail.com>
2018-10-03 20:03:01 -0600

    Add Heroku env var

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Database
 let urlDB = process.env.NODE_ENV === 'dev' ?
     'mongodb://localhost:27017/cafe' : 
-    'mongodb://cafe-user:C473User!@ds121753.mlab.com:21753/cafe';
+    process.env.MONGO_URL;
 process.env.URLDB = urlDB;
